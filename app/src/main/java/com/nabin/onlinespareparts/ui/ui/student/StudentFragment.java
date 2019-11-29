fd67943a14671ce5b00c570012ac116c6f6451db
nabin <nabin.sht.ns@gmail.com>
2019-11-29 06:53:27 +0545

    design changed

--- app/src/main/java/com/nabin/onlinespareparts/ui/ui/student/StudentFragment.java
+++ app/src/main/java/com/nabin/onlinespareparts/ui/ui/student/StudentFragment.java
@@ -72,7 +72,7 @@ public class StudentFragment extends Fragment {
                     saddress.setText("");
                     sage.setText("");
                 } else {
-                    Toast.makeText(getContext(), "Please select gender", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getContext(), " select gender", Toast.LENGTH_SHORT).show();
                 }
 
 
